83a0420edcdf0b3226f3d1c231439d69aa4243a7
Pablo Guala <gualapab@gmail.com>
2023-09-13 22:46:16 -0300

    v2 test get

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 let api = "https://crudcrud.com/api/adc54d7744e946cd8ffc1851accabb6d/grupo275";
 let contenedor = document.getElementById("contenedor");
 
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(mostrarRegistro, 1500);
+});
+
+
+
 //function mostrarRegistro(data, index) {
-    //  contenedor.innerHTML = JSON.stringify(data[index]);
-    //}
+//  contenedor.innerHTML = JSON.stringify(data[index]);
+//}
 function mostrarRegistro() {
     contenedor.innerHTML = "";
     fetch(api)
       .then((response) => {
         return response.json();
       })
-      .then((data) => {
-        console.log(data);
-        for (const i of data) {
-          contenedor.innerHTML += `<h5>ID generado: ${i._id}</h5>
-            <p>Nombre: ${i.nombre} ${i.apellido}</p>
-            <p>Grupo: ${i.grupo}</p>
-            <p>Sala: ${i.sala}</p>`;
-        }
-      });
+   .then((data) => {
+      console.log(data);
+      for (const dato of data) {
+        contenedor.innerHTML += `
+          <h4>ID generado: ${dato._id}</h4>
+          <p>Nombre: ${dato.nombre} ${dato.apellido}</p>
+          <p>Grupo: ${dato.grupo}</p>
+          <p>Sala: ${dato.sala}</p>`;
+      }
+    });
   }
+  
+  
 
 //function eliminarRegistro(i) {
 //  fetch(api + _id, {
@@ -36,7 +45,3 @@ function mostrarRegistro() {
 //    }
 //    setInterval(mostrarRegistro(apiEntero, indexRegistro), 1500);
 //  });
-
-document.addEventListener('DOMContentLoaded', () => {
-    setInterval(mostrarRegistro, 1500);
-});
